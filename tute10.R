41e88ea5bd303bd405ccf298ed16cbb60285d3b7
Rehar Subba <35126921@student.murdoch.edu.au>
2024-05-17 15:15:26 +0800

    10

--- tute10.R
+++ tute10.R
@@ -1,1 +1,26 @@
-data = read.csv("C:\Users\35126921\Downloads\customers.csv")
+data = read.csv("customer.csv", header=TRUE)
+head(data)
+str(data)
+
+x <- factor(data$Sex)
+str(x)
+data$Sex <- x
+str(data)
+
+data2 = scale(
+  data[, -c(1, 4)]
+)
+
+head(data2)
+
+hc = hclust(
+  dist(data2,
+       method="euclidean"),
+  method = "single"
+)
+
+hc
+
+plot(hc,
+     hang = -0.1,
+     cex = 0.6)
