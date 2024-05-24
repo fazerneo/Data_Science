8f0ac666554565be7c436a37f4be8c33ecff7fde
Rehar Subba <35126921@student.murdoch.edu.au>
2024-05-24 14:06:25 +0800

    tute10

--- tute10.R
+++ tute10.R
@@ -24,3 +24,42 @@ hc
 plot(hc,
      hang = -0.1,
      cex = 0.6)
+
+fit <- cutree(hc, k = 4)
+
+fit
+
+table(fit)
+
+plot(hc, hang = -1)
+
+
+rect.hclust(hc, k = 4, border = "red")
+abline(h = 0.96, col = "blue")
+
+data3 <- data.frame(data2, sex=x)
+str(data3)
+
+hc = hclust(
+  dist(data3,
+       method="euclidean"),
+  method = "ward.D2"
+)
+
+hc
+
+plot(hc,
+     hang = -0.1,
+     cex = 0.6)
+
+fit <- cutree(hc, k = 4)
+
+fit
+
+table(fit)
+
+plot(hc, hang = -1)
+
+
+rect.hclust(hc, k = 3, border = "red")
+abline(h = 5.5, col = "blue")
